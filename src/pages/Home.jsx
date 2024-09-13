cd1f8b9a1e37d714cc2585ca619d6b339823d97b
ScottKinnear <gr8whtninjassk@gmail.com>
2024-09-12 17:25:14 -0700

    Home page built

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import axios from "axios";
+import MovieCard from '../components/MovieCard.jsx';
+
+const Home = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const apiKey = 'a654bc3d';
+
+    const searchMovies = async (e) => {
+        try {
+            e.preventDefault();
+            const response = await axios.get(`https://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`);
+            setMovies(response.data.Search || [])
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
+    return (
+        <div className="home-container">
+            <form onSubmit={searchMovies}>
+                <input type="text" 
+                    value={searchTerm} 
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search movies..." 
+                />
+                <button type="submit">Search</button>
+            </form>
+            <div className="movie-list">
+                {movies.map((movie) => {
+                    <MovieCard key={movie.imdbID} movie={movie} />
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Home
